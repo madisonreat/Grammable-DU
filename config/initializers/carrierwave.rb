81b9096925e769aabd4a0908e2344d1ecf3448ad
Madison Cooper <madisondcooper@gmail.com>
2019-07-08 00:45:32 +0000

    changing application.yml

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,17 @@
+# config/initializers/carrierwave.rb
+
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
-  config.fog_credentials = {
-    provider:              'AWS',            
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],          
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']       
+  config.storage    = :aws
+  config.aws_bucket = ENV["AWS_BUCKET"]
+  config.aws_acl    = "public-read"
+
+  config.aws_credentials = {
+      access_key_id:     ENV["AWS_ACCESS_KEY"],
+      secret_access_key: ENV["AWS_SECRET_KEY"],
+      region:            ENV["AWS_REGION"]
   }
+  if Rails.env.development?  
+    config.cache_dir = '/home/vagrant/uploads_tmp/tmp/uploads'  
+    config.root = '/home/vagrant/uploads_tmp/tmp'
+  end
+end
